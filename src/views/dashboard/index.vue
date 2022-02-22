337baaa1aba923dd4ccc40147206e7f53c01bd05
cp3hnu <cp3hnu@gmail.com>
2022-02-22 15:22:37 +0800

    init

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data() {
+    return {}
+  },
+  methods: {
+    click(index) {
+      
+    }
+  }
+}
+</script>
+
+<style scoped></style>
