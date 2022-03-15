f4bd1bba6e3f33ed5cfdf185a52bc6872b9fb159
cp3hnu <cp3hnu@gmail.com>
2022-03-15 16:31:57 +0800

    feat: add semantic-release

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'hello-world-rule': [2, 'always']
+  },
+  plugins: [
+    {
+      rules: {
+        'hello-world-rule': ({ subject }) => {
+          const HELLO_WORLD = 'Hello World'
+          return [subject.includes(HELLO_WORLD), `Your subject should contain ${HELLO_WORLD} message`]
+        }
+      }
+    }
+  ]
 }
